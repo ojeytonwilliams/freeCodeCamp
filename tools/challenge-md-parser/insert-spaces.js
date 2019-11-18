143428c725105c736ec8764b8e6997bf310cfdc9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2019-11-18 15:36:31 +0100

    fix: cleaned up insert-spaces and added comments

--- tools/challenge-md-parser/insert-spaces.js
+++ tools/challenge-md-parser/insert-spaces.js
@@ -4,6 +4,37 @@ var raw = require('hast-util-raw');
 var findAndReplace = require('hast-util-find-and-replace');
 var toHtml = require('hast-util-to-html');
 
+/* Currently the challenge parser behaves differently depending on whether a
+section starts with an empty line or not.  If it does not, the parser interprets
+single new-line (\n) characters as paragraph breaks.  It also does not parse
+markdown syntax (such as `).  This makes formatting challenges harder than it
+needs to be, since normal markdown rules do not apply (some of the time!)
+
+For example
+<section id='instructions'>
+Sentence1.
+Sentence2 `var x = 'y'`.
+...
+
+becomes
+
+
+Sentence1.
+
+Sentence2 `var x = 'y'`.
+
+
+in the challenge, but should become
+
+
+Sentence1. Sentence2 <code>var x = 'y'</code>.
+
+---
+
+This file converts the instructions and descriptions.  After this there will be
+no need to handle the case where the first line is not empty and markdown syntax
+will alway work.  The linter can check that the first blank line exists.
+*/
 function plugin() {
   return transformer;
 
@@ -11,15 +42,19 @@ function plugin() {
     return visit(tree, 'html', visitor);
 
     function visitor(node) {
+      // 'html' nodes contain un-parsed html strings, so we first convert them
+      // to hast and then parse them to produce a syntax tree (so we can locate
+      // and modify the instructions and description)
       const section = raw(toHast(node, { allowDangerousHTML: true }));
-      // TODO: check that the section *doesn't* already have a blank line at the
-      // start, otherwise this could mess up the formatting.
       if (
         section.type === 'element' &&
         (section.properties.id === 'instructions' ||
           section.properties.id === 'description')
       ) {
-        console.log('section', section);
+        // section contains the section tag and all the text up to the first
+        // blank line.  This replaces single line breaks with empty lines, so
+        // that the section text that previously required special treatment
+        // becomes standard markdown.
         findAndReplace(section, '\n', '\n\n');
 
         // This will be used, once, to convert old challenges to the new
