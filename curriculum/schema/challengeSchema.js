2b282a61173fba5b0182d47d156f8d90f7579253
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-10-05 18:59:30 +0200

    fix: update challenge schema with ids

--- curriculum/schema/challengeSchema.js
+++ curriculum/schema/challengeSchema.js
@@ -14,6 +14,7 @@ const fileJoi = Joi.object().keys({
   tail: Joi.string().allow(''),
   seed: Joi.string().allow(''),
   contents: Joi.string().allow(''),
+  id: Joi.string().allow(''),
   history: [Joi.array().items(Joi.string().allow('')), Joi.string().allow('')]
 });
 
@@ -85,6 +86,8 @@ const schema = Joi.object().keys({
   tests: Joi.array().items(
     // public challenges
     Joi.object().keys({
+      // TODO: Remove allow('') and make required() once they all have ids.
+      id: Joi.string().allow(''),
       text: Joi.string().required(),
       testString: Joi.string()
         .allow('')
